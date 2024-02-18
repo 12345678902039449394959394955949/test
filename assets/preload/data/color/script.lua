7b9d927c150ff1726962604df999394ab5cc7e88
Vlad666exe <123743719+Vlad666exe@users.noreply.github.com>
2024-02-18 12:41:27 +0200

    Add files via upload

---
+++ assets/preload/data/color/script.lua
@@ -0,0 +1,19 @@
+function onCountdownTick(swagCounter)
+
+if swagCounter == 1 then  -- Ready
+
+playSound('trophy/1', 2)
+end
+
+if swagCounter == 2 then -- Set
+
+playSound('trophy/2', 2)
+end
+
+if swagCounter == 3 then -- GO
+
+playSound('trophy/3', 2)
+
+end
+
+end
