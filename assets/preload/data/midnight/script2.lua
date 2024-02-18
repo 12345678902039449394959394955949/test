8cc29c17fb509c3b27f110d9c98962bdc67c367f
Vlad666exe <123743719+Vlad666exe@users.noreply.github.com>
2024-02-18 13:05:05 +0200

    Add files via upload

---
+++ assets/preload/data/midnight/script2.lua
@@ -0,0 +1,6 @@
+function onBeatHit() 
+	   if (curBeat % 4 == 0) then
+    doTweenZoom('screenZoom', 'camHUD', 1.04, 0.02, 'linear');
+	 setProperty('camGame.zoom', getProperty('camGame.zoom') + 0.045)
+	   end
+end
